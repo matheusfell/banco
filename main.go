ab0a6bcfa841c101a21c34098ebefdbc0cd712da
Matheus Fellipe <bentomatheus48@gmail.com>
2023-02-15 10:42:25 -0300

    estudo buliano go

--- main.go
+++ main.go
@@ -10,19 +10,20 @@ type ContaCorrente struct {
 }
 
 func main() {
-	contaDoMatheus := ContaCorrente{titular: "Matheus", numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
+	//contaDoMatheus := ContaCorrente{titular: "Matheus", numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
 
-	contaDaJennie := ContaCorrente{"Jennifer", 699, 654321, 250.0}
+	contaDaJennie := ContaCorrente{"Jennifer", 689, 654321, 250.0}
+	contaDaJennie2 := ContaCorrente{"Jennifer", 699, 654321, 250.0}
 
-	fmt.Println(contaDoMatheus)
-	fmt.Println(contaDaJennie)
+	// fmt.Println(contaDoMatheus)
+	fmt.Println(contaDaJennie == contaDaJennie2)
 
-	var contaDoLucas *ContaCorrente
-	contaDoLucas = new(ContaCorrente)
-	contaDoLucas.titular = "Lucas"
-	contaDoLucas.numeroAgencia = 612
-	contaDoLucas.numeroConta = 321456
-	contaDoLucas.saldo = 12.0
+	//var contaDoLucas *ContaCorrente
+	//contaDoLucas = new(ContaCorrente)
+	//contaDoLucas.titular = "Lucas"
+	//contaDoLucas.numeroAgencia = 612
+	//contaDoLucas.numeroConta = 321456
+	//contaDoLucas.saldo = 12.0
 
-	fmt.Println(*contaDoLucas)
+	//fmt.Println(*contaDoLucas)
 }
